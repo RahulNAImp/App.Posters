ac91d897f9c02f6343212874a850ab6f921327af
Rahul Nnandanwar <rahulnandanwar32@gmail.com>
2025-10-07 18:45:06 +0530

    initial commit with mongo db

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,10 +1,6 @@
 // Controller for user-related handlers
 
-const getAllUsers = (req, res) => {
+exports.getAllUsers = (req, res) => {
 	// return a simple JSON response for now
     res.send("Router is running");
-};
-
-module.exports = {
-	getAllUsers,
 };
